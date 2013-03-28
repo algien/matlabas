5adb4f566482d34a4932efdafb949a5114dd3233
Rita Andruskaite <rita.andruskaite@gmail.com>
2013-03-28 12:52:21 +0200

    padarytas grafikas2

--- grafikas2.m
+++ grafikas2.m
@@ -1,2 +1,8 @@
-x^2=[-10:0.2:10];%padaryti, kad breztu dvimate funkcija: surf, mesh komandas naudoti
-plot(x^2,sincos2(x^2)); %plot netinka
+%x^2=[-10:0.2:10];%padaryti, kad breztu dvimate funkcija: surf, mesh komandas naudoti
+%plot(x^2,sincos2(x^2)); %plot netinka
+[x,y] = meshgrid (-10:0.2:10);
+f=sin(x)+cos(y)
+surf(x,y,f)
+%x,y
+%funkcija apsirasom
+%sakom, kad ja bresu pagal vusus x ir y
